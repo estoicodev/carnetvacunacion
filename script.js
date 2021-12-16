7e20ea719589d29e85d3ef35d0cb9c5b3baa67b2
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-12-16 17:57:47 -0500

    Add opacity when popup appear and eventListener keydown Escape to close popup

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const qrButton = document.getElementById("btnQr"),
 qrButton.addEventListener("click", appearPopup);
 popupClose.addEventListener("click", disappearPopup);
 
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    popup.classList.add("hidden");
+    popup.style.backgroundColor = "none";
+  }
+});
+
 function appearPopup() {
   popup.classList.remove("hidden");
+  popup.style.backgroundColor = "rgba(0,0,0,0.7)";
 }
 function disappearPopup() {
   popup.classList.add("hidden");
+  popup.style.backgroundColor = "none";
 }
-
-console.log("Funciono");
